ceefa07d952c00252ae2de2b69423a9cf7bf316b
Andreu <>
2020-09-15 17:12:09 +0200

    Updated gvars with the correct variables
    -Commented code
    -Obsolete variables out

--- gvars.py
+++ gvars.py
@@ -12,26 +12,25 @@ MAX_WORKERS = 10 # max threads at a time
 gainRatio = 1.5 # takeProfit = -stopLoss*gainRatio
 stopLossMargin = 0.05 # extra margin for the stop loss
 
-operEquity = 10000 # defines the target amount per execution
-limitOrderMargin = 0.1# defines the offset for the limit orders
+operEquity = 10000 # defines the target amount per execution ($)
+limitOrderMargin = 0.1 # percentage that defines the offset for the limit orders
 
 # YOUR API KEYS AT ALPACA GO HERE!
 API_KEY = ""
 API_SECRET_KEY = ""
 ALPACA_API_URL = "https://paper-api.alpaca.markets"
 
+# this block checks whether you have your keys written or not
 if API_KEY is "" or API_SECRET_KEY is "":
     print('\n\n##### \n\nPlease get an API key at the Alpaca website! \n\n##### \n\n')
     raise ValueError
 
 ################################################################ ATTEMPTS ->
-# max iteration attempts
+# max iteration attempts for the different actions
 maxAttempts = {
             'SO':5, # SUBMIT ORDER
             'CP':5, # CHECK POSITION
             'CO':5, # CANCEL ORDER
-            'GP':5, # GET POSITION
-            'FA':3, # FETCH ASSETS
             'LHD1':10, # LOAD HISTORICAL DATA 1
             'LHD2':20 # LOAD HISTORICAL DATA 2
             }
@@ -49,40 +48,29 @@ fetchItval = {
             'big':'30Min'
             }
 
+# timeouts that will kill a process
 timeouts = {
-        'operation':40*60*60, # main operation
-        'posEntered':8*60*60, # position entered
         'GT':0 # if 0, it discards a bad general trend instantly
         }
 
-# waiting time for each iteration
+# waiting time before repeating each iteration
 sleepTimes = {
-                'operation':60,
                 'GT': 10*60, # general trend
                 'IT': 2*60, # instant trend
                 'RS': 60, # RSI
-                'FA': 3, # fetch assets
                 'ST': 60, # stochastic every minut
-                'CO': 10, # check order every 10 secons
-                'SO': 5, # submit order every 5 secons
+                'CO': 10, # check order every 10 seconds
+                'SO': 5, # submit order every 5 seconds
                 'LH': 5, # load_historical_data
                 'PF': 10, # price fetch (current price)
                 'CP': 10, # check position, to check if it entered
                 'GS': 60, # get slope inside enter position
-                'UA': 10*60, # unlock assets
-                'CL': 2
+                'UA': 10*60 # unlock assets
                 }
 
 ################################################################ PATHS ->
 home = str(Path.home())
 
-FILES_FOLDER = home + '/tbot_files/'
-RAW_ASSETS = './_raw_assets.csv'
+FILES_FOLDER = home + '/tbot_files_test/'
+RAW_ASSETS = './_raw_assets.csv' # you should have this list at the same folder than everything
 LOGS_PATH = FILES_FOLDER + 'logs/'
-
-################################################################ ASSET PARAMS ->
-# filtering parameters at the asset handler
-filterParams = {
-    'MIN_SHARE_PRICE':30, #dollars
-    'MIN_AVG_VOL':0.5, #millions of dollars
-    }
